0fc5160c01b82b112808a2b844e0967f7a94aa50
DariuszPiskorowski <L00179768@atu.ie>
2024-05-11 16:02:57 +0100

    Initial commit-push the code to github repo

---
+++ mqtt_influx_bridge/bridge.js
@@ -0,0 +1,61 @@
+const mqtt = require('mqtt');
+const { InfluxDB, Point } = require('@influxdata/influxdb-client');
+
+// current config InfluxDB
+const influxDBUrl = 'http://influxdb:8086';
+const token = 'ecfIkGdIqwB-dEoIHT722esCaFnnYYdFN7JNLvsVbEHnZixuRVZOJSuJEq-yTP7qruXdZ99TwVrDZIeI4C1ltw==';
+const org = 'ATU';
+const bucket = 'AtuProject';
+
+const clientInflux = new InfluxDB({ url: influxDBUrl, token: token });
+
+
+// config MQTT
+const mqttBrokerUrl = 'mqtt://AIHome.local';
+const options = {
+ // clientId: 'influxBridge',
+  username: 'admin',
+  password: 'atu!3'
+};
+const mqttClient = mqtt.connect(mqttBrokerUrl, options);
+
+const subscriptionTopic = '#'; // Subscribe all topics
+
+mqttClient.on('connect', () => {
+  console.log('Connected to MQTT Broker');
+  mqttClient.subscribe(subscriptionTopic, () => {
+    console.log(`Subscription of all topics (${subscriptionTopic})`);
+  });
+});
+
+mqttClient.on('message', (topic, message) => {
+  console.log(`Received msg from ${topic}: ${message.toString()}`);
+
+  // Recording to influx
+  const writeApi = clientInflux.getWriteApi(org, bucket);
+  const point = new Point('mqtt_data').tag('topic', topic);
+
+ if (topic.includes('Temperature')) {
+    const value = parseFloat(message);
+  if(!isNaN(value)){
+    point.floatField('value', value);
+   } else {
+     console.error(`Received non-numeric data on topic ${topic}: ${message}`);
+     return;
+   }
+  }else{
+    point.stringField('message', message);
+  }
+
+  writeApi.writePoint(point);
+  writeApi
+    .close()
+    .then(() => {
+      console.log('Wrote data to InfluxDB');
+    })
+    .catch((err) => {
+      console.error(`Error during writing data to InfluxDB: ${err}`);
+    });
+});
+
+
---
+++ web-server/server.js
@@ -0,0 +1,111 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const mqtt = require('mqtt');
+const basicAuth = require('express-basic-auth');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// Fragment kodu odpowiedzialny za zarządzanie stanem urządzeń
+const deviceStates = {};
+const deviceTimeouts = {};
+
+// Uwierzytelnianie Basic Auth dla HTTP
+const authUsers = { 'admin': 'atu!3' };
+app.use(basicAuth({
+    users: authUsers,
+    challenge: true,  // Powoduje wyskoczenie okna dialogowego do logowania
+    realm: 'MQTTServer'
+}));
+
+// Opcje połączenia MQTT z uwierzytelnianiem
+const mqttOptions = {
+    host: '192.168.111.1',
+    port: 1883, // Zwykle MQTT korzysta z portu 1883 bez szyfrowania, zmień jeśli używasz innego
+    username: 'admin',
+    password: 'atu!3'
+};
+
+// Połączenie z serwerem MQTT
+const mqttClient = mqtt.connect(mqttOptions);
+
+mqttClient.on('connect', () => {
+    console.log("Connected to MQTT broker");
+    mqttClient.subscribe('#');
+    
+});
+
+mqttClient.on('message', (topic, message) => {
+    const parts = topic.split('/');
+    const device = parts[0]; // 'Boiler' or 'Heating'
+    const type = parts[1]; // 'Relay' or 'Temperature'
+    const id = parts[2]; // 'relay1', 'relay2', or 'sensorId'
+    const value = message.toString();
+
+    if (!deviceStates[device]) {
+        deviceStates[device] = {};
+    }
+    deviceStates[device][id] = { type, value };
+
+    // Reset timeout
+    clearTimeout(deviceTimeouts[device]);
+    deviceTimeouts[device] = setTimeout(() => {
+        console.log(`Removing inactive device: ${device}`);
+        delete deviceStates[device];
+        io.emit('removeDevice', { device });
+    }, 180000); // 3 minutes
+
+    io.emit('data', { device, type, id, value });
+});
+
+io.on('connection', (socket) => {
+    console.log('A client connected');
+
+    Object.keys(deviceStates).forEach(device => {
+        Object.keys(deviceStates[device]).forEach(id => {
+            const { type, value } = deviceStates[device][id];
+            socket.emit('data', { device, type, id, value });
+        });
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+
+    socket.on('switchRelay', (data) => {
+        const { device, id, action } = data;
+        // Przygotowanie tematu MQTT i wiadomości
+        const topic = `${device}/Relay/${id}`;
+        const message = action === 'on' ? 'on' : 'off';
+
+        // Publikacja do brokera MQTT
+        mqttClient.publish(topic, message, {}, (err) => {
+            if (err) {
+                console.log("Error publishing to MQTT:", err);
+            } else {
+                console.log(`Published to MQTT topic "${topic}" message "${message}"`);
+            }
+        });
+    });
+
+    socket.on('removeDevice', (data) => {
+        const { device } = data;
+        if (deviceStates[device]) {
+            delete deviceStates[device];
+            socket.emit('removeDevice', { device }); // Emit to all clients might be needed
+            console.log(`Device ${device} removed after inactivity.`);
+        }
+    });
+
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+const PORT = process.env.PORT || 2000;
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
